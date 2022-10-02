623363b7442baebb5ba455fcf6a193667806d61a
Paul Thomas <paulthomas2107@gmail.com>
2022-10-02 13:01:08 +0100

    More....

---
+++ app.js
@@ -0,0 +1,22 @@
+function showCountries() {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://restcountries.com/v3.1/all', true);
+
+  xhr.onload = function () {
+    if (xhr.status == 200) {
+      let countries = JSON.parse(this.response);
+
+      countries.forEach((country) => {
+        console.log(country);
+        let countryCard = document.createElement('div');
+        const countryCardImage = document.createElement('img');
+        countryCard.innerHTML = country.name.common;
+        countryCardImage.src = country.flags.png;
+        countryCard.appendChild(countryCardImage);
+        document.getElementById('feed').appendChild(countryCard);
+      });
+    }
+  };
+
+  xhr.send();
+}
